1d3e414c9462f37e98fb8052dbb9ac3499c99537
philipthedeveloper <philipowolabi79@gmail.com>
2025-05-04 19:57:58 +0100

    Update logger file

--- dist/src/logging/logger/winston.js
+++ dist/src/logging/logger/winston.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.logger = exports.LoggerLevels = void 0;
 const winston_1 = require("winston");
 const chalk_1 = __importDefault(require("chalk"));
-const path_1 = __importDefault(require("path"));
 var LoggerLevels;
 (function (LoggerLevels) {
     LoggerLevels["ERROR"] = "error";
@@ -29,27 +28,12 @@ const colorize = winston_1.format.printf(({ level, message, timestamp }) => {
                     : "white";
     return `${timestamp} ${chalk_1.default[color](level)}: ${chalk_1.default[color](message)}`;
 });
-const plainTextFormat = winston_1.format.printf(({ level, message, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`;
-});
-const env = (process.env.NODE_ENV || "development").trim();
-const logFileName = `${env}.combined.log`;
-const errorLogFileName = `${env}.error.log`;
 exports.logger = (0, winston_1.createLogger)({
     level: "debug",
     format: winston_1.format.combine(winston_1.format.timestamp(), winston_1.format.simple()),
     transports: [
         new winston_1.transports.Console({
             format: winston_1.format.combine(winston_1.format.timestamp(), colorize),
-        }),
-        new winston_1.transports.File({
-            filename: path_1.default.resolve(process.cwd(), env === "development" ? "src" : "dist/src", "logging", "logs", "errors", errorLogFileName),
-            level: "error",
-            format: winston_1.format.combine(winston_1.format.timestamp(), plainTextFormat),
-        }),
-        new winston_1.transports.File({
-            filename: path_1.default.resolve(process.cwd(), env === "development" ? "src" : "dist/src", "logging", "logs", "global", logFileName),
-            format: winston_1.format.combine(winston_1.format.timestamp(), plainTextFormat),
         }),
     ],
 });
--- src/logging/logger/winston.ts
+++ src/logging/logger/winston.ts
@@ -1,6 +1,5 @@
 import { createLogger, format, transports } from "winston";
 import chalk from "chalk";
-import path from "path";
 
 export enum LoggerLevels {
   ERROR = "error",
@@ -26,43 +25,12 @@ const colorize = format.printf(({ level, message, timestamp }) => {
   return `${timestamp} ${chalk[color](level)}: ${chalk[color](message)}`;
 });
 
-const plainTextFormat = format.printf(({ level, message, timestamp }) => {
-  return `${timestamp} ${level}: ${message}`;
-});
-
-const env = (process.env.NODE_ENV || "development").trim();
-const logFileName = `${env}.combined.log`;
-const errorLogFileName = `${env}.error.log`;
-
 export const logger = createLogger({
   level: "debug",
   format: format.combine(format.timestamp(), format.simple()),
   transports: [
     new transports.Console({
       format: format.combine(format.timestamp(), colorize),
-    }),
-    new transports.File({
-      filename: path.resolve(
-        process.cwd(),
-        env === "development" ? "src" : "dist/src",
-        "logging",
-        "logs",
-        "errors",
-        errorLogFileName
-      ),
-      level: "error",
-      format: format.combine(format.timestamp(), plainTextFormat),
-    }),
-    new transports.File({
-      filename: path.resolve(
-        process.cwd(),
-        env === "development" ? "src" : "dist/src",
-        "logging",
-        "logs",
-        "global",
-        logFileName
-      ),
-      format: format.combine(format.timestamp(), plainTextFormat),
     }),
   ],
 });
